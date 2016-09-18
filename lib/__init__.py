a3f2d1597b489b9e6e1407711d3cbc41bf7e9b13
jamesalbert <jalbert1@uci.edu>
2016-09-18 14:05:23 -0700

    app refactoring

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,53 +1,97 @@
+"""
+UniModMaker provides an easy way to create mods for Unitale
+
+try it at http://jimmyjam5000.com
+"""
+
 from configparser import ConfigParser, ExtendedInterpolation
 from jinja2 import Environment, FileSystemLoader
 from shutil import copytree, rmtree, move
 from zipfile import ZipFile, ZIP_DEFLATED
 import os
 
-def validate(conf, **kwargs):
-    for key, value in kwargs.iteritems():
-        if key in dict(conf.items('separable')):
-            items = value.split(',')
-            pat = '{0}' if items[0].isdigit() else '"{0}"'
-            kwargs[key] = ','.join([pat.format(x.strip()) for x in items])
-        if key not in dict(conf.items('not spaceable')):
-            kwargs[key] = kwargs[key].replace(' ', '\\n')
-	if key in dict(conf.items('radio')):
-            kwargs[key] = 'true' if value == 'on' else 'false'
-    return kwargs
 
-def zip_mod(path, zip_handle):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            zip_handle.write(os.path.join(root, file))
+class UniModHelper(object):
+    """provides methods to create a unitale mod"""
+    def __init__(self, **kwargs):
+        self.init_conf()
+        self.get_paths(kwargs['modname'])
+        copytree(self.resources_path, self.new_mod_path)
+        kwargs = self.validate(**kwargs)
+        self.fill_templates(**kwargs)
+        self.rename_waves(kwargs['possible_attacks'])
+        self.zip_mod(self.new_mod_path)
+        rmtree(self.new_mod_path)
 
-def fill_template(env, template, **kwargs):
-    return env.get_template(template).render(**kwargs)
+    def get_paths(self, modname):
+        """
+        get paths to resources and the mod to be created
 
-def init_conf():
-    conf = ConfigParser()
-    conf._interpolation = ExtendedInterpolation()
-    conf.read('./conf.ini')
-    return conf
+        :param modname: the name of the mod
+        """
+        mods_path = self.conf.get("base_paths", "mods")
+        self.resources_path = self.conf.get("base_paths", "resources")
+        self.new_mod_path = '{0}/{1}'.format(mods_path, modname)
 
-def initialize(**kwargs):
-    conf = init_conf()
-    res = conf.get("base_paths", "resources")
-    mpt = conf.get("base_paths", "mods")
-    mod = '{0}/{1}'.format(mpt, kwargs['modname'])
-    try:
-        copytree(res, mod)
-    except OSError:
-        return False;
-    kwargs = validate(conf, **kwargs)
-    env = Environment(loader=FileSystemLoader(res), trim_blocks=True)
-    for label, path in conf.items("paths"):
-        with open('{0}/{1}'.format(mod, path), 'w+') as new_file:
-            new_file.write(fill_template(env, path, **kwargs))
-    for i, wave in enumerate(kwargs['possible_attacks'].split(',')):
-	move('{0}/Lua/Waves/wave{1}.lua'.format(mod, i+1), '{0}/Lua/Waves/{1}.lua'.format(mod, wave.replace('"', '')))
-    zip_handle = ZipFile('{0}.zip'.format(mod), 'w', ZIP_DEFLATED)
-    zip_mod(mod, zip_handle)
-    zip_handle.close()
-    rmtree(mod)
-    return True;
+    def rename_waves(self, possible_attacks):
+        """
+        rename wave filenames
+
+        :param possible_attacks: the possible attacks (waves)
+        """
+        for i, wave in enumerate(possible_attacks.split(',')):
+            old = '{0}/Lua/Waves/wave{1}.lua'.format(self.new_mod_path, i+1)
+            new = '{0}/Lua/Waves/{1}.lua'.format(self.new_mod_path, wave.replace('"', ''))
+            move(old, new)
+
+    def validate(self, **kwargs):
+        """
+        validate breaks up form fields to their respective data structures
+
+        :param kwargs: dictionary of form items
+        """
+        for key, value in kwargs.items():
+            if key in dict(self.conf.items('separable')):
+                items = value.split(',')
+                pat = '{0}' if items[0].isdigit() else '"{0}"'
+                kwargs[key] = ','.join([pat.format(x.strip()) for x in items])
+            if key not in dict(self.conf.items('not spaceable')):
+                kwargs[key] = kwargs[key].replace(' ', '\\n')
+            if key in dict(self.conf.items('radio')):
+                kwargs[key] = 'true' if value == 'on' else 'false'
+        return kwargs
+
+    def zip_mod(self, path):
+        """
+        archive the created mod folder
+
+        :param path: path to mod folder
+        :param zip_handle: file handle of zip archive
+        """
+        zip_handle = ZipFile('{0}.zip'.format(self.new_mod_path), 'w', ZIP_DEFLATED)
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                zip_handle.write(os.path.join(root, file))
+        zip_handle.close()
+
+    def fill_templates(self, **kwargs):
+        """
+        fill templates (encounter.lua, etc) with specified values
+
+        :param kwargs: dictionary of form fields
+        """
+        resfs = FileSystemLoader(self.resources_path)
+        env = Environment(loader=resfs, trim_blocks=True)
+        for label, path in self.conf.items("paths"):
+            with open('{0}/{1}'.format(self.new_mod_path, path), 'w+') as new_file:
+                new_file.write(
+                    env.get_template(path).render(**kwargs)
+                )
+
+    def init_conf(self):
+        """
+        get a dictionary representing conf.ini
+        """
+        self.conf = ConfigParser()
+        self.conf._interpolation = ExtendedInterpolation()
+        self.conf.read('./conf.ini')
--- server.py
+++ server.py
@@ -1,16 +1,18 @@
 from flask import current_app, Flask, render_template, request, send_from_directory
-from lib import initialize, init_conf
+from lib import UniModHelper
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = init_conf().get('base_paths', 'mods')
+app.config['UPLOAD_FOLDER'] = './mods'
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'POST':
-        initialize(**request.form.to_dict())
-        return send_from_directory(directory=os.path.join(current_app.root_path, 'mods'), filename='{0}.zip'.format(request.form['modname']))
+        UniModHelper(**request.form.to_dict())
+        mods_path = os.path.join(current_app.root_path, 'mods')
+        zip_file = '{0}.zip'.format(request.form['modname'])
+        return send_from_directory(directory=mods_path, filename=zip_file)
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
